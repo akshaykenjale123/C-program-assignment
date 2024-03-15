ac87ab5df3ae64e582d1a0a286c5a089d8dcdb96
Akshay_kenjale <akshay.kenjale1711@gmail.com>
2024-03-15 13:48:20 +0530

    assignment

---
+++ assignment 3/q18.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,product;
+	printf("enter the number:");
+	scanf("%d",&product);
+	for(i=1;i<=10;i++)
+	{
+	 for(j=1;j<=10;j++)
+	 
+	 {
+	    
+	    printf("%d\t",i *j);
+	 }
+	 printf("\n");
+	}
+	return 0;
+}
---
+++ assignment 3/q19.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,product;
+	printf("enter the number:");
+	scanf("%d",&product);
+	for(i=1;i<=10;i++)
+	{
+	 for(j=3;j<=6;j++)
+	 
+	 {
+	    
+	    printf("%d\t",i *j);
+	 }
+	 printf("\n");
+	}
+	return 0;
+}
